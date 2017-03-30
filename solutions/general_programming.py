46f457fa79f1cac0550ab1f370cf8c5d4c5ce99b
Grier Phillips <GrierPhillips@gmail.com>
2017-03-30 10:12:21 -0600

    Update to conform with PEP257.

--- solutions/general_programming.py
+++ solutions/general_programming.py
@@ -1,21 +1,23 @@
-'''
-general_programming.py: This module contains answers to the quesions in the
-    General Programming section of the 100 Data Science questions set.
-'''
+"""
+General Programming Solutions.
+
+This module contains answers to the quesions in the General Programming section
+of the 100 Data Science questions set.
+"""
 
 from itertools import repeat
 
-'''
+"""
 1.  Write a function that converts a dictionary of equal length lists into a
     list of dictionaries.
 
     {'a': [1, 2, 3], 'b': [3, 2, 1]}
     => [{'a': 1, 'b': 3}, {'a': 2, 'b': 2}, {'a': 3, 'b': 1}]
-'''
+"""
 
 
 def dict_to_list(dictionary):
-    '''
+    """
     Convert a dictionary of equal length lists into a list of dictionaries.
 
     Args:
@@ -27,7 +29,7 @@ def dict_to_list(dictionary):
     Example:
         >>> dict_to_list({'a': [1, 2, 3], 'b': [3, 2, 1]})
         [{'a': 1, 'b': 3}, {'a': 2, 'b': 2}, {'a': 3, 'b': 1}]
-    '''
+    """
     zippers = []
     for key, value in dictionary.items():
         zippers.append(zip(repeat(key), value))
